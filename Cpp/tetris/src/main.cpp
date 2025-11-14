ffe22a77033fee8dd4e8ba8b2ba1f8fd065f1345
Ghost <Ghost@qq.com>
2025-11-14 15:03:36 +0800

    完善环境

--- Cpp/tetris/src/main.cpp
+++ Cpp/tetris/src/main.cpp
@@ -1,1 +1,28 @@
 #include <SFML/Graphics.hpp>
+
+int main() {
+    // 使用英文标题
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "My Tetris!");
+    
+    // 创建一个矩形方块
+    sf::RectangleShape block({100.f, 100.f});
+    block.setPosition({100.f, 100.f});
+    block.setFillColor(sf::Color::White);
+
+    // 游戏循环
+    while (window.isOpen()) {
+        // 事件处理
+        while (auto event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {
+                window.close();
+            }
+        }
+
+        // 渲染
+        window.clear();
+        window.draw(block);
+        window.display();
+    }
+
+    return 0;
+}
